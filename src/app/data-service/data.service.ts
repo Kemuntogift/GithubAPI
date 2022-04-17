9d23ae184f957bb722df2fa771d3b7ca2e5d0ebd
Kemunto Gift <giftoyugi@gmail.com>
2022-04-17 19:42:26 +0300

    import httpclient for request

--- src/app/data-service/data.service.ts
+++ src/app/data-service/data.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { User } from '../user';
+import { Repo } from '../repo';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  user!: User;
+  repo!: Repo;
+  repos = [];
+
   getData: any;
 
-  constructor() { }
+  constructor() {}
 }
