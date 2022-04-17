38f64c79749d843f1db074be7b0ada7f3aef512c
Kemunto Gift <giftoyugi@gmail.com>
2022-04-17 16:59:26 +0300

    move app.component.html elements to landing page component

---
+++ src/app/lpage/lpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lpage',
+  templateUrl: './lpage.component.html',
+  styleUrls: ['./lpage.component.css']
+})
+export class LpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
