875052759cead74585bc23dc3fdd410097ce8617
Kemunto Gift <giftoyugi@gmail.com>
2022-04-18 19:13:09 +0300

    add navbar component for routing and router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { LpageComponent } from './lpage/lpage.component';
+const routes: Routes = [
+  { path: 'lpage', component: LpageComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,11 @@ import { SearchFormComponent } from './search-form/search-form.component';
 import { LpageComponent } from './lpage/lpage.component';
 import { DataService } from './data-service/data.service';
 import { DateCountPipe } from './date-count.pipe';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchFormComponent, LpageComponent, DateCountPipe],
+  declarations: [AppComponent, SearchFormComponent, LpageComponent, DateCountPipe, AboutComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
